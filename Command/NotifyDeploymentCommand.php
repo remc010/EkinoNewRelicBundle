8c7af783711b017d8ae1770e53a3898992a608cd
Remco Westerduin <rwesterduin@12gobiking.nl>
2024-01-18 18:16:08 +0100

    Remove php7 support;
    Fix: The command defined in "Ekino\NewRelicBundle\Command\NotifyDeploymentCommand" cannot have an empty name.

--- Command/NotifyDeploymentCommand.php
+++ Command/NotifyDeploymentCommand.php
@@ -14,18 +14,21 @@ declare(strict_types=1);
 namespace Ekino\NewRelicBundle\Command;
 
 use Ekino\NewRelicBundle\NewRelic\Config;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'newrelic:notify-deployment',
+    description: 'Notifies New Relic that a new deployment has been made.',
+)]
 class NotifyDeploymentCommand extends Command
 {
     public const EXIT_NO_APP_NAMES = 1;
     public const EXIT_UNAUTHORIZED = 2;
     public const EXIT_HTTP_ERROR = 3;
-
-    protected static $defaultName = 'newrelic:notify-deployment';
 
     private $newrelic;
 
@@ -57,7 +60,6 @@ class NotifyDeploymentCommand extends Command
                     'Text annotation for the deployment — notes for you', null
                 ),
             ])
-            ->setDescription('Notifies New Relic that a new deployment has been made')
         ;
     }
 
